ef7477e9df5196a04d9113ca6652b389dbaa1e12
NFR176 <nevaeh.foster-ricket@ada.ac.uk>
2025-02-18 18:12:19 +0000

    final commit equipment management

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -24,3 +24,24 @@ SELECT
     next_maintenance_date                 
 FROM equipment
 WHERE next_maintenance_date BETWEEN DATE('now') AND DATE('now', '+30 days');  -- Filters dates within the next 30 days
+
+--3.2--
+
+SELECT 
+    type AS equipment_type,  -- 'type AS equipment_type' just renames 'type' to 'equipment_type' for easier reading. 'COUNT(*) AS count' counts how many pieces of equipment there are for each type and names it 'count'.
+    COUNT(*) AS count                    
+FROM equipment
+WHERE location_id IS NOT NULL           
+GROUP BY type;         
+
+--3.3--
+SELECT
+    e.type AS equipment_type,  
+    AVG( 
+        (strftime('%s', 'now') - strftime('%s', e.purchase_date)) / 86400
+    ) AS avg_age_days -- The average age in days
+    -- '/' divides the difference between the current date and purchase date (in seconds) by 86400 to convert from seconds to days.
+FROM
+    equipment e  --  'equipment' table, aliased as 'e'
+GROUP BY
+    e.type;  -- Grouping by equipmeint type to get the average for each type
